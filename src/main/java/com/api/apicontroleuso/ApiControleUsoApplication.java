ed3fe670e41f08fcb5cf8a36ebfabee95698d4d0
Eduardo <emicheletto67@gmail.com>
2024-02-19 11:33:38 -0300

    First Commit! Projeto criado em Java com o Spring Initializantion. Com as Dependecias SpringDataJpa, SpringWeb, SpringSecurity.
    A app tem como objeto receber as informaçoes de uso da App web e salvar os dados do usuario.

---
+++ src/main/java/com/api/apicontroleuso/ApiControleUsoApplication.java
@@ -0,0 +1,13 @@
+package com.api.apicontroleuso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleUsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleUsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/apicontroleuso/ApiControleUsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apicontroleuso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleUsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
