42a3ead44443da345784f24801d0801f3215db05
Eduardo <emicheletto67@gmail.com>
2024-03-05 14:18:33 -0300

    Add Swagger ao projeto, liberado acesso ao swagger no AUTH_WHITELIST dentro do SecurityConfig

--- src/main/java/com/api/apicontroleuso/config/SecurityConfig.java
+++ src/main/java/com/api/apicontroleuso/config/SecurityConfig.java
@@ -1,22 +1,15 @@
 package com.api.apicontroleuso.config;
 
-import com.mysql.cj.protocol.x.XAuthenticationProvider;
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.util.AntPathMatcher;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
